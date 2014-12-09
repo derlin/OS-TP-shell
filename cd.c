96b53af958d188120341c86f8d571ffdeb6052e3
Lucy Linder <lucy.derlin@gmail.com>
2014-12-09 16:40:53 +0100

    simplify the cd function

--- cd.c
+++ cd.c
@@ -24,46 +24,37 @@ void cd( int argc, char * argv[ ] )
     }
 
     char * arg = argv[1];
-    char * pwd, *oldpwd, * npwd;
+    char * pwd, * npwd;
+
     char buff[1024];
 
     pwd = EVget(PWD);
-    oldpwd = EVget(OLDPWD);
-
 
     if(strcmp(arg, "-") == 0)
     {
-        strcpy(buff, oldpwd);
-        npwd = buff;
+        npwd = EVget(OLDPWD);
     }
     else if(arg[0] == '~')
     {
         char * home = EVget("HOME");
-        strcpy(buff, home);
-        strcpy(buff + strlen(home), arg+1);
+        strncpy(buff, home, 1024);
+        strncpy(buff + strlen(home), arg+1, 1024);
         npwd = buff;
-    }
-    else if(arg[0] == '/')
-    {
-        npwd = arg;
     }
     else
     {
-        char relpath[1024];
-        sprintf(relpath, "%s/%s", pwd, arg);
-        realpath(relpath, buff);
-        npwd = buff;
+        npwd = arg;
     }
 
 
-//    struct stat s;set
-//    stat(npwd, &s);
-//    if(S_ISDIR(s.st_mode))
-//    {
     if(chdir(npwd) == 0)
     {
         int ok = EVset(OLDPWD, pwd);
+        // get absolute path
+        npwd = get_current_dir_name();
         ok &= EVset(PWD, npwd);
+        free(npwd);
+
         if(ok) printf("CWD: %s\n", EVget(PWD));
     }
     else
